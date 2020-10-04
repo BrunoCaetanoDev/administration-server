6b0176f9d67264e17d1bd4a450b68a7e183e106a
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2020-10-04 21:51:35 +0100

    Initial commit release 0.0.1-SNAPSHOT

---
+++ src/test/java/com/bruno/caetano/dev/administrationserver/AdministrationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.caetano.dev.administrationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

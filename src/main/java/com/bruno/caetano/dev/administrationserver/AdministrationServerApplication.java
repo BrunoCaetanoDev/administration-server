9b0b0505c1f6bc3d90068f36caeefce2d24ca64c
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2021-01-19 17:52:05 +0000

    close release 0.0.1

--- src/main/java/com/bruno/caetano/dev/administrationserver/AdministrationServerApplication.java
+++ src/main/java/com/bruno/caetano/dev/administrationserver/AdministrationServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdministrationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdministrationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdministrationServerApplication.class, args);
+    }
 
 }

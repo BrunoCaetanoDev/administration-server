b97ec73b2e8013d1e6b096f95ebdb9f9b5adfe32
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2021-01-20 21:56:01 +0000

    [feature/CNECP-2]:[Minor adjustments to pom.xml]

--- src/main/java/com/bruno/caetano/dev/administrationserver/AdministrationServerApplication.java
+++ src/main/java/com/bruno/caetano/dev/administrationserver/AdministrationServerApplication.java
@@ -1,10 +1,10 @@
 package com.bruno.caetano.dev.administrationserver;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableAdminServer
+@EnableEurekaClient
 @SpringBootApplication
 public class AdministrationServerApplication {
 
